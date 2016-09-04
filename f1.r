1e7a320c95ecab2843ff946c86b1511cd414bbed
Marcus Nunes <marcus.nunes@gmail.com>
2016-09-04 18:49:00 -0300

    Update f1.r

--- f1.r
+++ f1.r
@@ -1,10 +1,11 @@
 # bibliotecas necessarias
+rm(list=ls())
 
 library(XML)
 library(RCurl)
 library(stringr)
+library(caret)
 library(ggplot2)
-library(stats)
 
 # url de interesse
 
@@ -13,18 +14,34 @@ url <- "https://en.wikipedia.org/wiki/List_of_Formula_One_drivers"
 # baixar os dados localmente e extrair as tabelas da pagina
 
 tabs <- getURL(url)
-tabs <- readHTMLTable(tabs, stringsAsFactors=FALSE)
+
+# obter os nomes dos pilotos corretamente
+
+myFun <- function(x){
+  if(length(y <- getNodeSet(x, ".//a")) > 0){
+    # return data.frame
+    title <- xpathSApply(x, ".//a", fun=xmlGetAttr, name="title")
+    href  <- xpathSApply(x, ".//a", fun=xmlGetAttr, name="href")
+    value <- xpathSApply(x, ".//a", fun=xmlValue)
+    return(paste(value, collapse = ","))
+  }
+  xmlValue(x, encoding = "UTF-8")
+}
+
+pilotos.nomes <- readHTMLTable(tabs, stringsAsFactors=FALSE, elFun=myFun)[[3]][, 1]
+pilotos.nomes <- pilotos.nomes[-length(pilotos.nomes)]
 
 # criar o data frame pilotos, apenas com a tabela que interessa
 
-pilotos <- tabs[[3]]
-tail(pilotos)
+tabs    <- getURL(url)
+pilotos <- readHTMLTable(tabs, stringsAsFactors=FALSE)
+pilotos <- pilotos[[3]]
 pilotos <- pilotos[-dim(pilotos)[1], ] # retira a ultima linha da tabela
 
 dim(pilotos)
 names(pilotos)
 
-pilotos.bk <- pilotos
+head(pilotos)
 
 # limpar a tabela
 
@@ -36,36 +53,32 @@ for (j in 5:10){
 
 pilotos.numeros <- pilotos[, c("Championships", "Entries", "Starts", "Poles", "Wins", "Podiums", "Fastest laps")]
 
-# corrige os nomes dos pilotos
-
-rownames(pilotos.numeros) <- gsub("^.+?,\\s*(\\w+)\\1(.+?)$", "\\1\\2", pilotos$Name)
+rownames(pilotos.numeros) <- pilotos.nomes
 
 # estatisticas sobre as colunas da tabela
 
 summary(pilotos.numeros)
 
-# correlacao entre pole positions e vitorias
-
-cor.test(pilotos.numeros$Poles, pilotos.numeros$Wins, method="spearman")
-ggplot(pilotos.numeros, aes(x=Poles, y=Wins)) + geom_point(size=2, alpha=0.25) + xlab("Pole Positions") + ylab("Vit�rias")
-
 # clusterizacao feita por pilotos, tentando encontrar os mais parecidos entre si
 
 # normalizacao das variaveis - sugestão de Gustavo Fontoura
-pilotos.numeros.norm <- pilotos.numeros
-pilotos.numeros.norm$Championships <- pilotos.numeros.norm$Championships/sum(pilotos.numeros$Championships)
-pilotos.numeros.norm$Entries <- pilotos.numeros.norm$Entries/sum(pilotos.numeros$Entries)
-pilotos.numeros.norm$Starts <- pilotos.numeros.norm$Starts/sum(pilotos.numeros$Starts)
-pilotos.numeros.norm$Poles <- pilotos.numeros.norm$Poles/sum(pilotos.numeros$Poles)
-pilotos.numeros.norm$Wins <- pilotos.numeros.norm$Wins/sum(pilotos.numeros$Wins)
-pilotos.numeros.norm$Podiums <- pilotos.numeros.norm$Podiums/sum(pilotos.numeros$Podiums)
+
+pilotos.numeros.norm                <- pilotos.numeros
+pilotos.numeros.norm$Championships  <- pilotos.numeros.norm$Championships/sum(pilotos.numeros$Championships)
+pilotos.numeros.norm$Entries        <- pilotos.numeros.norm$Entries/sum(pilotos.numeros$Entries)
+pilotos.numeros.norm$Starts         <- pilotos.numeros.norm$Starts/sum(pilotos.numeros$Starts)
+pilotos.numeros.norm$Poles          <- pilotos.numeros.norm$Poles/sum(pilotos.numeros$Poles)
+pilotos.numeros.norm$Wins           <- pilotos.numeros.norm$Wins/sum(pilotos.numeros$Wins)
+pilotos.numeros.norm$Podiums        <- pilotos.numeros.norm$Podiums/sum(pilotos.numeros$Podiums)
 pilotos.numeros.norm$`Fastest laps` <- pilotos.numeros.norm$`Fastest laps`/sum(pilotos.numeros$`Fastest laps`)
+
+# calculo das distancias entre os pilotos
 
 pilotos.dist <- dist(pilotos.numeros.norm, method="manhattan")
 
-pilotos.cluster <- hclust(pilotos.dist,method = "average")
-pp <- as.dendrogram(pilotos.cluster)
-plot(pp)
+pilotos.cluster <- hclust(pilotos.dist, method="average")
+
+plot(pilotos.cluster)
 
 # pca
 
@@ -74,20 +87,14 @@ summary(pca)
 
 # graficos
 
-# 5 clusters
+pca.plot <- as.data.frame(pca$x)
 
-plot(pca$x[, 1], pca$x[, 2], col=as.numeric(cutree(pilotos.cluster, k=5)), xlab="PC 1", ylab="PC 2")
+# 3 clusters - melhor resultado
 
-# 4 clusters
-
-plot(pca$x[, 1], pca$x[, 2], col=as.numeric(cutree(pilotos.cluster, k=4)), xlab="PC 1", ylab="PC 2")
-
-# 3 clusters
-
-plot(pca$x[, 1], pca$x[, 2], col=as.numeric(cutree(pilotos.cluster, k=3)), xlab="PC 1", ylab="PC 2")
+ggplot(pca.plot, aes(x=pca.plot[, 1], y=pca.plot[, 2], color=as.factor(cutree(pilotos.cluster, k=3)))) + geom_point(size=2) + xlab("Componente Principal 1") + ylab("Componente Principal 2") + scale_color_discrete(name ="Grupos de Pilotos", labels=c("Pilotos Normais", "Pilotos Bons", "Michael Schumacher"))
 
 # identificacao dos pilotos
 
-pilotos.numeros[cutree(pilotos.cluster, k=3)==1, ]
-pilotos.numeros[cutree(pilotos.cluster, k=3)==2, ]
-pilotos.numeros[cutree(pilotos.cluster, k=3)==3, ]
+rownames(pilotos.numeros[cutree(pilotos.cluster, k=3)==1, ])
+rownames(pilotos.numeros[cutree(pilotos.cluster, k=3)==2, ])
+rownames(pilotos.numeros[cutree(pilotos.cluster, k=3)==3, ])
